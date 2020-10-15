dd74f2f163f78a9bc488f675c507f5c5e6379ec4
Hp <Hp@Shoxed>
2020-10-15 21:15:50 +0700

    Create View (Menu)

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -7,7 +7,7 @@ package View;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-
+import Model.*;
 /**
  *
  * @author Hp
@@ -19,9 +19,33 @@ public class Menu {
     }
     
     private void showMenuAwal() {
+        ArrayList<User> user = new ArrayList<>();
         int cek = JOptionPane.NO_OPTION;
         while(cek != JOptionPane.YES_OPTION){
             int number = Integer.parseInt(JOptionPane.showInputDialog(null,"Travel Bis Emen\n 1.Register\n 2.Login"));
+            if(number == 1){
+                String nama = JOptionPane.showInputDialog("Nama : ");
+                String password = JOptionPane.showInputDialog("Password : ");
+                String alamat = JOptionPane.showInputDialog("Alamat : ");
+                String noHP = JOptionPane.showInputDialog("No.HP : ");
+                int umur = Integer.parseInt(JOptionPane.showInputDialog("Umur : "));
+                String ktp = JOptionPane.showInputDialog("No.KTP : ");
+                user.add(new Member(nama, password, alamat, noHP, umur, ktp));
+            }else if(number == 2){
+                String nama = JOptionPane.showInputDialog("Nama : ");
+                String password = JOptionPane.showInputDialog("Password : ");
+                for(User i : user){
+                    if(i.getNama().equals(nama) && i.getPassword().equals(password)){
+                        JOptionPane.showMessageDialog(null, "Welcome Back, " + i.getNama() + " !");
+                        showMenuLanjut();
+                        break;
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Nama dan/atau Password Salah!");
+                    }
+                }
+            }else{
+                JOptionPane.showMessageDialog(null,"Input Salah!");
+            }
         }
         cek = JOptionPane.showConfirmDialog(null,"Ulang Program?");
     }
@@ -29,7 +53,7 @@ public class Menu {
     private void showMenuLanjut(){
         int cek = JOptionPane.NO_OPTION;
         while(cek != JOptionPane.YES_OPTION){
-            int number = Integer.parseInt(JOptionPane.showInputDialog(null,"Travel Bis Emen\n 1.List Perjalanan\n 2.Pesan Tiket"));
+            int number = Integer.parseInt(JOptionPane.showInputDialog(null,"Travel Bis Emen\n 1.List Perjalanan \n2.Pesan Tiket \n3.Pembatalan Tiket \n4.Riwayat Pemesanan \n5.Cek Saldo OVO \n6.TopUp OVO"));
         }
     }
     
