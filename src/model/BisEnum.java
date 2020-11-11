5ad6925bb1bc0a32cc4c38a5fd949f1252f122f8
chouXsaber <ramaparama9@gmail.com>
2020-11-11 11:03:07 +0700

    Tambah bis enum

---
+++ src/model/BisEnum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author lenovo
+ */
+public enum BisEnum {
+    
+    VIP,reguler,ekonomi
+    
+}
