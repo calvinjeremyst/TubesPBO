2940078eea01f6e875033b42bd123d566d6a58ee
Hp <Hp@Shoxed>
2020-11-11 09:49:22 +0700

    Update List Order

--- src/Model/ListOrder.java
+++ src/Model/ListOrder.java
@@ -5,30 +5,36 @@
  */
 package Model;
 
-import java.util.ArrayList;
-import Model.*;
 /**
  *
  * @author Hp
  */
 public class ListOrder {
-    private String idOrder;
+    private int idOrder;
     private String tanggalOrder;
-    ArrayList<User> listUser;
-    ArrayList<ListBus> bis;
-    ArrayList<Rute> rute;
-    ArrayList<Kursi> kursi;
+    private int banyakPenumpang;
+    private Member member;
+    private Rute rute;
+    private TransaksiPembayaran transaksi;
+
+    public ListOrder(){
+        
+    }
     
-    public ListOrder(String idOrder, String tanggalOrder) {
+    public ListOrder(int idOrder, String tanggalOrder, int banyakPenumpang, Member member, Rute rute, TransaksiPembayaran transaksi) {
         this.idOrder = idOrder;
         this.tanggalOrder = tanggalOrder;
+        this.banyakPenumpang = banyakPenumpang;
+        this.member = member;
+        this.rute = rute;
+        this.transaksi = transaksi;
     }
 
-    public String getIdOrder() {
+    public int getIdOrder() {
         return idOrder;
     }
 
-    public void setIdOrder(String idOrder) {
+    public void setIdOrder(int idOrder) {
         this.idOrder = idOrder;
     }
 
@@ -39,26 +45,39 @@ public class ListOrder {
     public void setTanggalOrder(String tanggalOrder) {
         this.tanggalOrder = tanggalOrder;
     }
-    
-    public void listPerjalanan(){
-        
+
+    public int getBanyakPenumpang() {
+        return banyakPenumpang;
+    }
+
+    public void setBanyakPenumpang(int banyakPenumpang) {
+        this.banyakPenumpang = banyakPenumpang;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Rute getRute() {
+        return rute;
+    }
+
+    public void setRute(Rute rute) {
+        this.rute = rute;
+    }
+
+    public TransaksiPembayaran getTransaksi() {
+        return transaksi;
+    }
+
+    public void setTransaksi(TransaksiPembayaran transaksi) {
+        this.transaksi = transaksi;
     }
     
-    public double bayarTiket(){
-        double total = 0;
-        return total;
-    }
     
-    public void pesanTiket(){
-        bayarTiket();
-    }
-    
-    public void pembatalanTiket(){
-        for(User user : listUser){
-            Member member = (Member) user;
-            member.riwayatPemesanan();
-            
-        }
-    }
     
 }
