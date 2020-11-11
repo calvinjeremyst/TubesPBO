577cdd45515bb939b85e0bd8b4a6f757b63b998f
chouXsaber <ramaparama9@gmail.com>
2020-11-11 10:44:36 +0700

    Update Member

--- src/model/Member.java
+++ src/model/Member.java
@@ -13,25 +13,21 @@ import java.util.ArrayList;
  *
  * @author Hp
  */
-public class Member extends User implements interfaceTubes{
+public class Member extends User implements interfaceTubes {
    
     private int ID_Member;
     private int umur;
-    private String KTP; 
+    private String KTP;
     
-   
     public Member(){
         
     }
     
-    public Member(int ID_User,String username, String password,String repassword, String alamat, String noHp,int ID_Member,int umur, String KTP
-            ) {
+    public Member(int ID_User,String username, String password,String repassword, String alamat, String noHp,int ID_Member,int umur, String KTP) {
         super(ID_User,username, password,repassword, alamat, noHp);
         this.ID_Member = ID_Member;
         this.umur = umur;
         this.KTP = KTP;
-      
-        
     }
 
     public int getID_Member() {
@@ -42,57 +38,66 @@ public class Member extends User implements interfaceTubes{
         this.ID_Member = ID_Member;
     }
     
-    
+    @Override
     public int getID_User() {
         return ID_User;
     }
 
+    @Override
     public void setID_User(int ID_User) {
         this.ID_User = ID_User;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
 
+    @Override
     public void setUsername(String username) {
         this.username = username;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
 
+    @Override
     public void setPassword(String password) {
         this.password = password;
     }
 
+    @Override
     public String getRepassword() {
         return repassword;
     }
 
+    @Override
     public void setRepassword(String repassword) {
         this.repassword = repassword;
     }
 
+    @Override
     public String getAlamat() {
         return alamat;
     }
 
+    @Override
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
 
+    @Override
     public String getNoHp() {
         return noHp;
     }
 
+    @Override
     public void setNoHp(String noHp) {
         this.noHp = noHp;
     }
     
-    
-
     public int getUmur() {
         return umur;
     }
@@ -108,11 +113,5 @@ public class Member extends User implements interfaceTubes{
     public void setKTP(String KTP) {
         this.KTP = KTP;
     }
-    
-    
-
-   
-    
-   
     
 }
